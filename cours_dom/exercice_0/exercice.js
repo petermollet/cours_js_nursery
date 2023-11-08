b37dfc37dcbcabdd50b08e57822171778daa5791
peter.mollet <pmollet@insy2s.fr>
2023-11-08 11:40:03 +0100

    correction exo 0

--- cours_dom/exercice_0/exercice.js
+++ cours_dom/exercice_0/exercice.js
@@ -1,3 +1,15 @@
-const estConnecte = false;
+const estConnecte = true;
 
 // Algorythme de modification du DOM:
+
+const messageUtilisateur = document.getElementById("message-utilisateur");
+
+if (!estConnecte) {
+    messageUtilisateur.textContent = "Veuillez vous connecter à l'application avant de continuer";
+    messageUtilisateur.classList.remove("text-primary");
+    messageUtilisateur.classList.add("text-danger");
+} else {
+    messageUtilisateur.textContent = "Bonjour et bienvenue sur votre compte !";
+    messageUtilisateur.classList.remove("text-danger");
+    messageUtilisateur.classList.add("text-primary");
+}
