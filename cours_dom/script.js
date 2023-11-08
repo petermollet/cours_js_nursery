1301ce6402daa57f6c09a6a5b8ff6dd5a602c25f
peter.mollet <pmollet@insy2s.fr>
2023-11-08 11:12:05 +0100

    ajout pdf du cours

--- cours_dom/script.js
+++ cours_dom/script.js
@@ -7,10 +7,14 @@ console.log(containers);
 let bonjours = document.getElementsByName("bonjour");
 console.log(bonjours);
 
-
-
-
 titre.textContent = "Bonjour le monde";
 
 let maDiv = document.getElementById("ma-div");
-maDiv.innerHTML = "<p>Ma balise insérer avec javascript</p>"
+maDiv.innerHTML = "<p>Ma balise insérer avec javascript</p>";
+
+titre.classList.add("m-3");
+titre.classList.add("border-bottom");
+
+maDiv.classList.remove("container");
+
+document.getElementById("mon-container").classList.toggle("container");
