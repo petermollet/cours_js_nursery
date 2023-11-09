88577a3f2b54fc3839be3407dabd5f46370ec31f
peter.mollet <pmollet@insy2s.fr>
2023-11-09 15:10:49 +0100

    DOM exo 5 - correction

--- cours_dom/exercice_5/script.js
+++ cours_dom/exercice_5/script.js
@@ -1,1 +1,24 @@
+const dark = document.getElementById("dark");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
 
+dark.addEventListener("click", function (e) {
+  const body = document.getElementsByTagName("body")[0];
+  body.classList.remove("yellowTheme");
+  body.classList.remove("greenTheme");
+  body.classList.add("darkTheme");
+});
+
+yellow.addEventListener("click", function (e) {
+  const body = document.getElementsByTagName("body")[0];
+  body.classList.remove("darkTheme");
+  body.classList.remove("greenTheme");
+  body.classList.add("yellowTheme");
+});
+
+green.addEventListener("click", function (e) {
+  const body = document.getElementsByTagName("body")[0];
+  body.classList.remove("yellowTheme");
+  body.classList.remove("darkTheme");
+  body.classList.add("greenTheme");
+});
