979e40c2382959219baf1f5a67c8e650a2b4a324
peter.mollet <pmollet@insy2s.fr>
2023-11-09 11:02:51 +0100

    DOM exo 1 - correction

--- cours_dom/exercice_1/script.js
+++ cours_dom/exercice_1/script.js
@@ -9,3 +9,24 @@ const livre = {
 };
 
 // ALGO pour insérer les données au dessus:
+
+// j'insère le text dans les balises text
+document.getElementById("titre").textContent = livre.titre;
+document.getElementById("auteur").textContent = livre.auteur;
+document.getElementById("description").textContent = livre.description;
+document.getElementById("date").textContent = livre.dateDeParution;
+document.getElementById("nombre-pages").textContent = livre.nombreDePages;
+
+// j'insère l'URL de l'image dans le src de la balise img
+document.getElementById("couverture").src = livre.image;
+
+// selon si disponible ou non j'affiche un texte différent avec une couleur de fond différente
+if (livre.estDisponible) {
+  // SI disponible
+  document.getElementById("disponible").textContent = "Disponible";
+  document.getElementById("disponible").classList.add("bg-success");
+} else {
+  // SINON indisponible
+  document.getElementById("disponible").textContent = "Non disponible";
+  document.getElementById("disponible").classList.add("bg-danger");
+}
