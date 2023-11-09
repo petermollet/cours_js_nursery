38b02c9b92d94c1032266a6c3a28df95531e35c5
peter.mollet <pmollet@insy2s.fr>
2023-11-09 13:43:58 +0100

    DOM exo 3

---
+++ cours_dom/evenements/example.js
@@ -0,0 +1,10 @@
+// écouteur d'événements
+
+// je récupère ma balise dont je veux récupèrer l'événement
+const monBoutton = document.getElementById("mon-boutton");
+
+// je rajoute un écouteur d'événement sur cette balise. ici du type click
+monBoutton.addEventListener("click", function(e) {
+    // L'exécution quand l'événement est enclenché
+    
+});
---
+++ cours_dom/evenements/script.js
@@ -0,0 +1,6 @@
+const monBoutton = document.getElementById("mon-boutton");
+
+monBoutton.addEventListener("click", function (e) {
+  document.getElementById("titre").textContent = "Bonjour";
+});
+
